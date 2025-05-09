6a2b9efcc3e6c690f20c67c7d4c350969d6a70b1
Tarasov Vladislav <467667@niuitmo.ru>
2025-05-09 11:34:08 +0300

    добавлена улучшенная структура TODO: написать json для тестов и собственно доделать сами тесты

---
+++ autotests/config.py
@@ -0,0 +1,20 @@
+# RabbitMQ connection parameters
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
+RABBITMQ_USER = 'admin'
+RABBITMQ_PASS = 'password'
+RABBITMQ_VHOST = '/'
+
+# BRT database connection parameters
+BRT_DB_HOST = 'localhost'
+BRT_DB_PORT = 5433
+BRT_DB_NAME = 'brtdb'
+BRT_DB_USER = 'postgres'
+BRT_DB_PASS = 'postgres'
+
+# HRS database connection parameters
+HRS_DB_HOST = 'localhost'
+HRS_DB_PORT = 5434
+HRS_DB_NAME = 'hrsdb'
+HRS_DB_USER = 'postgres'
+HRS_DB_PASS = 'postgres'
